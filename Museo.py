da9fe0076d40e34caca0383189a07892d882c270
gabrielrosales07 <gabriel.rosales@correo.unimet.edu.ve>
2025-08-08 10:05:47 -0400

    interfaz museo

--- Museo.py
+++ Museo.py
@@ -4,6 +4,22 @@ from Department import Department
 class Museo:
   
   def start(self):
+    self.iniciar_objetos()
+        
+        while True:
+            interfaz = input ("""Bienvenido al Museo MetroArt, escoja una opcion: 
+1- Buscar Obras
+2- Ver Detalles de Obras
+---> """)
+            if interfaz == "1":
+              self.show_obras()
+
+  def show_obras (self):
+        choice = input ("""Ingrese como desea ver las obras: 
+1- Por Departamento
+2- Por Nacionalidad del Autor
+3- Por Nombre del Autor
+---> """)
 
   def show_departments (self):
     for department in self.
