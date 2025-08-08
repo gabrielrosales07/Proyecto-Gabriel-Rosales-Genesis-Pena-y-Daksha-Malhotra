ff80280d716e2f9359052a43fa62921544ccab40
gabrielrosales07 <gabriel.rosales@correo.unimet.edu.ve>
2025-08-08 10:48:34 -0400

    indentacion arreglada

--- Museo.py
+++ Museo.py
@@ -15,55 +15,55 @@ class Museo:
         self.show_obras()
 
   def show_obras (self):
-        choice = input ("""Ingrese como desea ver las obras: 
+    choice = input ("""Ingrese como desea ver las obras: 
 1- Por Departamento
 2- Por Nacionalidad del Autor
 3- Por Nombre del Autor
 ---> """)
     
     if choice == "1":
-            self.show_departments()
-            id_departamento = input ("Ingrese el ID de un departamento: ")
+      self.show_departments()
+      id_departamento = input ("Ingrese el ID de un departamento: ")
+      
+      try:
+        department_found = False
+        for departamento in self.department:
+          if departamento.id == int(id_departamento):
+            department_found = True
+            break
             
-            try:
-                department_found = False
-                for departamento in self.department:
-                    if departamento.id == int(id_departamento):
-                     department_found = True
-                     break
-                    
-                if department_found:
-                    object_ids = objects_in_departments ()
-                    if len(object_ids) > 0:
-                        print("Obras encontradas con éxito")
-                        print(f"Se encontraron {len(object_ids)} obras. ")
+          if department_found:
+            object_ids = objects_in_departments ()
+            if len(object_ids) > 0:
+              print("Obras encontradas con éxito")
+              print(f"Se encontraron {len(object_ids)} obras. ")
+              
+              self.obras = []
+              for object_id in object_ids [:10]:
+                dic_obras = objects_details (object_id)
+                if dic_obras:
+                  self.obras.append(Obra( id_obra = dic_obras.get ("objectID"), titulo = dic_obras.get ("title"), autor = dic_obras.get ("artistDisplayName"), nacionalidad = dic_obras.get ("artistNationality"), fecha = dic_obras.get ("objectDate")))
 
-                        self.obras = []
-                        for object_id in object_ids [:10]:
-                            dic_obras = objects_details (object_id)
-                            if dic_obras:
-                                self.obras.append(Obra( id_obra = dic_obras.get ("objectID"), titulo = dic_obras.get ("title"), autor = dic_obras.get ("artistDisplayName"), nacionalidad = dic_obras.get ("artistNationality"), fecha = dic_obras.get ("objectDate")))
+              for obra in self.obras:
+                obra.show()
 
-                        for obra in self.obras:
-                            obra.show()
+            else:
+              print ("No se encontraron obras en este departamento")
 
-                    else:
-                        print ("No se encontraron obras en este departamento")
-
-                else:
-                    print ("Error: ID ingresado incorrecto")
+          else:
+            print ("Error: ID ingresado incorrecto")
             
-            except:
-                print ("Error: El ID del departamento debe ser un número")
+      except:
+        print ("Error: El ID del departamento debe ser un número")
 
   def show_departments (self):
-    for department in self.
-    print()
-    department.show()
+    for department in self.department:
+      print()
+      department.show()
 
   def iniciar_objetos (self):   
-        dic_departments = department_api() ["departments"]
-        self.department = []
+    dic_departments = department_api() ["departments"]
+    self.department = []
     
     for department in dic_departments:
       self.department.append (Department(department["departmentId"], department["displayName"]))
