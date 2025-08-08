182ad6a22758bff9b6dd6dce121302e03ce25ba2
dakshamalhotra <daksha.malhotra@correo.unimet.edu.ve>
2025-08-08 10:10:25 -0400

    Update Obra.py

--- Obra.py
+++ Obra.py
@@ -1,17 +1,3 @@
-def objects_in_departments ():
-    obj_departments = requests.get ("https://collectionapi.metmuseum.org/public/collection/v1/search")
-    try:
-        if obj_departments.status_code == 200:
-            db = obj_departments.json()
-            return db.get ("objectIDs", [])
-        
-        else: 
-            print (f"Se encontro un error al buscar por departamento. Codigo: {obj_departments.status_code}")
-            return []
-    
-    except:
-        print (f"Error de conexion, intentelo de nuev
-
 class Obra:
     def __init__(self, id_obra, titulo, autor, nacionalidad, fecha):
         self.id_obra = id_obra
